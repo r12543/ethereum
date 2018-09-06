e5646bda7dd7d4c408d57f580240fba4c062c709
Rajat Chaudhary <rajat.chaudhary@innoplexus.com>
2018-09-06 19:16:46 +0530

    redeploy the contract

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x847Af31c9B9246d779C196cc2505cF2b2480afcE"
+  "0x523E03003AB241b8b8cC342401387998db2FeC39"
 );
 
 export default instance;
